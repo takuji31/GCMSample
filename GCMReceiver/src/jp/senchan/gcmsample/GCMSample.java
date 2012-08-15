ff56d2a502489869524f6d21d02c8531eb193492
Nishibayashi Takuji <takuji31@gmail.com>
2012-08-15 12:33:43 +0900

    fix typo

--- GCMReceiver/src/jp/senchan/gcmsample/GCMSample.java
+++ GCMReceiver/src/jp/senchan/gcmsample/GCMSample.java
@@ -3,7 +3,8 @@ package jp.senchan.gcmsample;
 import jp.senchan.gcmkey.GCMKey;
 import android.app.Application;
 
-public class GCMSaple extends Application {
+public class GCMSample extends Application {
 	//Please change this
 	public static final String GCM_SENDER_ID = GCMKey.GCM_KEY;
+	public static final String BROADCAST_ACTION = "ACTION_GCM_SAMPLE_IntentService";
 }
